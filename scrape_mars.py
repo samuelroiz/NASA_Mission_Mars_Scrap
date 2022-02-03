3f316326745a396d0e549dfd59271c580a3c72fd
Sam Roiz <sroiz01@gmail.com>
2022-02-02 18:35:36 -0800

    Updated files. Finished scrape coding. Just need to finish scrape file.

--- scrape_mars.py
+++ scrape_mars.py
@@ -1,5 +1,5 @@
 from splinter import Browser
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup  as bs
 from webdriver_manager.chrome import ChromeDriverManager
 
 def scrape():
@@ -7,19 +7,59 @@ def scrape():
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=False)
 
-    listings = {}
-
-    url = "https://webscraper.io/test-sites/e-commerce/allinone/phones/touch"
+    mars_lattest_news = {}
+    url = "https://redplanetscience.com/"
     browser.visit(url)
 
     html = browser.html
-    soup = BeautifulSoup(html, "html.parser")
+    soup = bs(html, "html.parser")
 
-    listings["headline"] = soup.find("a", class_="title").get_text()
-    listings["price"] = soup.find("h4", class_="price").get_text()
-    listings["reviews"] = soup.find("p", class_="pull-right").get_text()
+    mars_lattest_news["news_title"] = soup.find("div", class_="content_title").get_text()
+    mars_lattest_news["news_p"] = soup.find("div", class_="article_teaser_body").get_text()
+    mars_lattest_news["news_date"] = soup.find("div", class_="list_date").get_text()
 
-    # Quit the browser
-    browser.quit()
+    # Set up Splinter
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
 
-    return listings
+    featured_img_url = "https://spaceimages-mars.com/"
+    browser.visit(featured_img_url)
+
+    html = browser.html
+    soup = bs(html, "html.parser")
+
+    relative_image_path = soup.find_all('img')[1]['src']
+    feature_image = featured_img_url + relative_image_path
+
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
+
+    mars_hemisphere_url = "https://marshemispheres.com/"
+    browser.visit(mars_hemisphere_url)
+
+    html = browser.html
+    soup = bs(html, "html.parser")
+
+    first_title = soup.find_all("h3")[0].get_text()
+    second_title = soup.find_all("h3")[1].get_text()
+    third_title = soup.find_all("h3")[2].get_text()
+    fourth_title = soup.find_all("h3")[3].get_text()
+
+    image_link = soup.find_all('img', class_="thumb")[0]['src']
+    first_img = mars_hemisphere_url + image_link
+
+    image_link = soup.find_all('img', class_="thumb")[1]['src']
+    second_img = mars_hemisphere_url + image_link
+
+    image_link = soup.find_all('img', class_="thumb")[2]['src']
+    third_img = mars_hemisphere_url + image_link
+
+    image_link = soup.find_all('img', class_="thumb")[3]['src']
+    fourth_img = mars_hemisphere_url + image_link
+
+    hemisphere_image_urls = [
+        {"title": first_title, "img_url": first_img},
+        {"title": second_title, "img_url": second_img},
+        {"title": third_title, "img_url": third_img},
+        {"title": fourth_title, "img_url":fourth_img},
+    ]
